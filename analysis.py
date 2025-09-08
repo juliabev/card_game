4bfd39028e9c2328400b3dfde18647e4208b298b
juliabev <jbbeverley@wm.edu>
2025-09-08 12:57:11 -0400

    edited main and analysis

--- analysis.py
+++ analysis.py
@@ -1,12 +1,22 @@
 import numpy as np
 
-player1 = [0,0,0]
+player1 = [1,1,1]
 
 
-def player2(n: int=3, low: int=0, high: int=25): # type hinting
+def getp2combo(list) -> list: # type hinting
     """
     Return a list of 3 numbers that gives the best probability of winning the 
     humble nishiyama randomness game
     """ # docstring
-
-    return np.random.randint(low=low, high=high+1, size=(n,2))
+    
+    for index, j in enumerate(player1):
+        player2 = [0,0,0]
+        if index == 1:
+            if j == 0:
+               player2[0] == 1
+            else:
+               player2[0] == 0
+        
+        player2[1] == player1[0]
+        player2[2] == player1[1]
+    return player2
--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 def main():
     print("Hello from card-game!")
     print("Hi from Kayla & Julia")
+    
 
 
 if __name__ == "__main__":
     main()
+
+player1 = [1,1,1]
+
+from analysis import getp2combo
+print(getp2combo(player1))
