b3eeb85a3816ec9641e93a91304edbf1ba54d5ae
22kaviles <97423052+22kaviles@users.noreply.github.com>
2025-09-15 13:51:44 -0400

    add files

---
+++ method2.py
@@ -0,0 +1,48 @@
+import numpy as np
+import random
+
+full_deck = [0]*26 + [1]*26
+#shuffled_deck = random.shuffle(full_deck)
+
+player1 = [0,0,0]
+
+def getp1combo() -> list:
+    '''
+    Gives a random player 1 combo in the form of a list
+    '''
+    for index in range(len(player1)):
+        player1[index] = random.choice((0,1))
+    return player1
+
+def getp2combo(list) -> list: # type hinting
+    """
+    Return a list of 3 numbers that gives the best probability of winning the 
+    humble nishiyama randomness game
+    """ 
+
+    a, b, c = player1
+    return [1 - b, a, b]
+
+def shuffle(list) -> list:
+    '''
+    Shuffle deck.
+    '''
+    shuffled_deck = full_deck.copy()
+    random.shuffle(shuffled_deck)
+    return shuffled_deck
+
+def cardgame2(list):
+    '''
+    When you give a deck of 
+    '''
+    for index, j in enumerate(shuffled_deck):
+        temp_window = [None]*3
+        temp_window = [index, index+1, index+2]
+
+
+        index += 3
+
+        
+        
+
+    
