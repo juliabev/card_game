92e18de2a3467b2914d128666e7cba9cb85a6e9f
juliabev <jbbeverley@wm.edu>
2025-09-19 13:53:41 -0400

    took our scoring

---
+++ score/main_score.py
@@ -0,0 +1,17 @@
+import math
+import analysis as a
+
+
+TOTAL_DECKS = 2_000_000
+BATCH_SIZE = 10_000
+OUT_DIR = "Data"
+
+# gen batches
+for i in range(math.ceil(TOTAL_DECKS / BATCH_SIZE)):
+    print(f"Running batch {i+1}/{TOTAL_DECKS//BATCH_SIZE} ...")
+    a.simulate_batch(i, BATCH_SIZE, OUT_DIR)
+
+# summarize results
+print("All batches complete. Creating summary.json...")
+a.summarize(OUT_DIR)
+print("Summary written to Data/summary.json")
